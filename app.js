b9a51b7e46d7bbfdd9095592ab96bb670ecfef0b
Gaurav Gupta <guptag@tcd.ie>
2021-03-07 17:37:33 +0000

    [NEW] delete-post, add-post

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const bodyParser = require("body-parser");
-
+const mongoose = require("mongoose");
+const Post = require("./models/post");
 const app = express();
+// grCS4cGnAM1tHt6a
+
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useNewUrlParser', true);
+mongoose.connect("mongodb+srv://gaurav:grCS4cGnAM1tHt6a@cluster0.bskds.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+.then( () => console.log("connected to DB."))
+.catch( err => console.log(err));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,31 +28,37 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-    const post = req.body;
-    console.log(post);
-    res.status(201).json({
-      message: 'Post added successfully'
+    const post = new Post({
+      title: req.body.title,
+      content: req.body.content
     });
+    post.save().then(result => {
+      res.status(201).json({
+        message: 'Post added successfully',
+        postId: result._id
+      });
+    })
 });
 
 app.get("/api/posts", (req, res, next) => {
-    
-    const posts = [
-      {
-        id: "fadf12421l",
-        title: "First server-side post",
-        content: "This is coming from the server"
-      },
-      {
-        id: "ksajflaj132",
-        title: "Second server-side post",
-        content: "This is coming from the server!"
-      }
-    ];
+  Post.find().then(documents => {
     res.status(200).json({
       message: "Posts fetched successfully!",
-      posts: posts
+      posts: documents
     });
+  });
 });
+
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post.deleteOne({
+    _id: req.params.id
+  }).then(result => {
+    console.log(result);
+    res.status(200).json({
+      message: "Post deleted"  
+    })
+  })
+  ;
+})
   
 module.exports = app;
