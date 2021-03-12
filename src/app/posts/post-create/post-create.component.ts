9a6dcc646519bb5a3d61d4a83a4d3cb3de4ec294
Gaurav Gupta <guptag@tcd.ie>
2021-03-12 14:31:29 +0000

    Add routing and editing

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Post } from '../post.model';
 import { PostService } from '../post.service';
 
@@ -12,19 +13,38 @@ export class PostCreateComponent implements OnInit {
 
   enteredTitle = '';
   enteredContent = ''; 
+  post: Post;
+  private mode = 'create';
+  private postId: string;
+  
 
-
-  constructor(public postsService: PostService) { }
+  constructor(public postsService: PostService, public route: ActivatedRoute) { }
 
 
   ngOnInit(): void {
+
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if(paramMap.has("postId")) {
+        this.mode = "edit";
+        this.postId = paramMap.get("postId");
+        this.post = this.postsService.getPost(this.postId);
+      } else {
+        this.mode = "create";
+        this.postId = null;
+      }
+    })
   }
 
-  onAddPost(form: NgForm) {
+  onSavePost(form: NgForm) {
     if(form.invalid) {
       return;
     }
-    this.postsService.addPost(form.value.title, form.value.content);
+    if(this.mode === "create") {
+      this.postsService.addPost(form.value.title, form.value.content);
+    } else {
+      this.postsService.updatePost(this.postId, form.value.title, form.value.content);
+    }
+    
     form.resetForm();
   }
 
--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -57,4 +57,20 @@ export class PostService {
         this.postsUpdated.next([...this.posts]);
       });
     }
-}
+
+    getPost(id: string) {
+      return {...this.posts.find(p => p.id === id)};
+    }
+
+    updatePost(id: string, title: string, content: string) {
+      const post: Post = {
+        id: id,
+        title: title,
+        content: content
+      };      
+      this.http.put("http://localhost:3000/api/posts/"+id, post)
+      .subscribe((response) => {
+        console.log(response);
+      })
+    }
+  }
