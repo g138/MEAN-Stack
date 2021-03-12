5bd3c1be99de7749620e7bd9cedff891820e9b3f
Gaurav Gupta <guptag@tcd.ie>
2021-03-12 14:33:05 +0000

    [NEW] Edit post

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
     );
     res.setHeader(
       "Access-Control-Allow-Methods",
-      "GET, POST, PATCH, DELETE, OPTIONS"
+      "GET, POST, PATCH, DELETE, OPTIONS, PUT"
     );
     next();
 });
@@ -39,6 +39,22 @@ app.post("/api/posts", (req, res, next) => {
       });
     })
 });
+
+app.put("/api/posts/:id", (req, res, next) => {
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  });
+  Post.updateOne({
+    _id: req.params.id,
+  }, post).then(result => {
+    console.log(result);
+    res.status(200).json({
+      message: 'Post updated successfully'
+    })
+  })
+})
 
 app.get("/api/posts", (req, res, next) => {
   Post.find().then(documents => {
