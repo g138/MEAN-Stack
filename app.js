2b0180333cd71d784d987847b27f758664243735
Gaurav Gupta <guptag@tcd.ie>
2021-03-15 19:06:56 +0000

    [New] seperate-post-api

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const Post = require("./models/post");
+const postsRouter = require("./routes/posts");
 const app = express();
 // grCS4cGnAM1tHt6a
 
@@ -27,54 +27,6 @@ app.use((req, res, next) => {
     next();
 });
 
-app.post("/api/posts", (req, res, next) => {
-    const post = new Post({
-      title: req.body.title,
-      content: req.body.content
-    });
-    post.save().then(result => {
-      res.status(201).json({
-        message: 'Post added successfully',
-        postId: result._id
-      });
-    })
-});
+app.use("/api/posts", postsRouter);
 
-app.put("/api/posts/:id", (req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content
-  });
-  Post.updateOne({
-    _id: req.params.id,
-  }, post).then(result => {
-    console.log(result);
-    res.status(200).json({
-      message: 'Post updated successfully'
-    })
-  })
-})
-
-app.get("/api/posts", (req, res, next) => {
-  Post.find().then(documents => {
-    res.status(200).json({
-      message: "Posts fetched successfully!",
-      posts: documents
-    });
-  });
-});
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  Post.deleteOne({
-    _id: req.params.id
-  }).then(result => {
-    console.log(result);
-    res.status(200).json({
-      message: "Post deleted"  
-    })
-  })
-  ;
-})
-  
 module.exports = app;
